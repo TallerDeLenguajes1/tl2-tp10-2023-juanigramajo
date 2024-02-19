fff4cfac6133f74b5cee8a83423084ca153c1cb9
Juani Gramajo <juanigramajo@hotmail.com>
2024-02-18 23:23:26 -0500

    Opciones de asignar usuario a tareas

---
+++ Controllers/TableroController.cs
@@ -0,0 +1,254 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tl2_tp10_2023_juanigramajo.Models;
+using tl2_tp10_2023_juanigramajo.ViewModels.Tableros;
+using tl2_tp10_2023_juanigramajo.ViewModels.Usuarios;
+
+namespace tl2_tp10_2023_juanigramajo.Controllers
+{
+    public class TableroController : Controller
+    {
+        private ITableroRepository _repositorioTablero;
+        private readonly ILogger<TableroController> _logger;
+
+
+        public TableroController(ILogger<TableroController> logger, ITableroRepository tableroRepository)
+        {
+            _logger = logger;
+            _repositorioTablero = tableroRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            try
+            {
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+
+                string rolUsuario = HttpContext.Session.GetString("Rol");
+                string idUser = HttpContext.Session.GetString("Id");
+
+                if(!isAdmin())
+                {
+                    List<Tablero> ListadoTableros = _repositorioTablero.OthersListByUser(Convert.ToInt32(idUser));
+                    List<Tablero> ListadoMisTableros = _repositorioTablero.ListByUser(Convert.ToInt32(idUser));
+                    List<Tablero> ListadoTareasEnOtroTablero = _repositorioTablero.ListByTareasEnOtroTablero(Convert.ToInt32(idUser));
+                    ListarTablerosViewModel listarTablerosVM = new ListarTablerosViewModel(ListadoTableros, ListadoMisTableros, ListadoTareasEnOtroTablero, idUser);
+
+                    if (ListadoTableros != null)
+                    {
+                        return View(listarTablerosVM);
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                } 
+                else 
+                {
+                    List<Tablero> ListadoTableros = _repositorioTablero.OthersListByUser(Convert.ToInt32(idUser));
+                    List<Tablero> ListadoMisTableros = _repositorioTablero.ListByUser(Convert.ToInt32(idUser));
+                    List<Tablero> ListadoTareasEnOtroTablero = _repositorioTablero.ListByTareasEnOtroTablero(Convert.ToInt32(idUser));
+                    ListarTablerosViewModel listarTablerosVM = new ListarTablerosViewModel(ListadoTableros, ListadoMisTableros, ListadoTareasEnOtroTablero, idUser);
+
+                    if (ListadoTableros != null)
+                    {
+                        return View(listarTablerosVM);
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+            
+        }
+
+
+        [HttpGet]
+        public IActionResult CrearTablero()
+        {   try
+            {
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+                if(!isAdmin())
+                {   
+                    TempData["ErrorMessage"] = "No tienes permisos para crear un tablero";
+                    return RedirectToAction("Index");
+                }
+
+                return View(new CrearTableroViewModel(Convert.ToInt32(HttpContext.Session.GetString("Id"))));                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+        }
+
+    
+        [HttpPost]
+        public IActionResult CrearTablero(CrearTableroViewModel crearTableroVM)
+        {   
+            try
+            {
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+                if(!isAdmin())
+                {   
+                    TempData["ErrorMessage"] = "No tienes permisos para crear un tablero";
+                    return RedirectToAction("Index");
+                }
+                if(!ModelState.IsValid) return RedirectToAction("CrearTablero");
+
+
+                Tablero tablero = new Tablero(crearTableroVM);
+                _repositorioTablero.Create(crearTableroVM.IdUsuarioPropietario, tablero);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+        }
+    
+
+        [HttpGet]
+        public IActionResult EditarTablero(int idTablero)
+        {
+            try
+            {
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+                if(!isAdmin())
+                {   
+                    TempData["ErrorMessage"] = "No tienes permisos para editar un tablero";
+                    return RedirectToAction("Index");
+                }
+
+                ModificarTableroViewModel modificarTableroVM = new ModificarTableroViewModel(_repositorioTablero.GetById(idTablero));
+
+                return View(modificarTableroVM);                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult EditarTablero(ModificarTableroViewModel modificarTableroVM)
+        {
+            try
+            {    
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+                if(!isAdmin())
+                {   
+                    TempData["ErrorMessage"] = "No tienes permisos para editar un tablero";
+                    return RedirectToAction("Index");
+                }
+                if(!ModelState.IsValid) return RedirectToAction("EditarTablero");
+
+                Tablero tablero2 = new Tablero(modificarTableroVM);
+                _repositorioTablero.Update(tablero2.Id, tablero2);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+        }
+
+        
+        public IActionResult DeleteTablero(int idTablero)
+        {
+            try
+            {
+                if (!isLogged())
+                {
+                    TempData["ErrorMessage"] = "Debes iniciar sesión para acceder a esta sección.";
+                    return RedirectToRoute(new { controller = "Login", action = "Index" });
+                }
+                if(!isAdmin())
+                {   
+                    TempData["ErrorMessage"] = "No tienes permisos para eliminar un tablero";
+                    return RedirectToAction("Index");
+                }
+                
+                _repositorioTablero.Remove(idTablero);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                TempData["ErrorMessage"] = ex.Message;
+                TempData["StackTrace"] = ex.StackTrace;
+                return RedirectToAction("Error");
+            }
+        }
+
+
+        private bool isLogged()
+        {
+            if (HttpContext.Session.GetString("Id") != null) 
+                return true;
+                
+            return false;
+        }
+
+
+        private bool isAdmin()
+        {
+            if (HttpContext.Session.GetString("Rol") == "Administrador") 
+                return true;
+                
+            return false;
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,60 @@
+# tl2-tp10-2023-juanigramajo
+
+## Explicación:
+
+### 1. Pestañas del navbar
+- Sin estar loggeado, únicamente puede acceder a crear un usuario o a login, todas las demás redireccionan a loggearse.
+- Al crear un usuario únicamente se puede asignar el rol de un operador.
+
+### 2. Permisos de sesión
+- Un admin:
+    - Puede crear usuarios con cualquier rol.
+    - Puede editar y eliminar usuarios (no puede eliminar su propio usuario ni bajar su propio rango de rol).
+    - Puede crear, editar y eliminar tableros.
+    - Puede crear, editar y eliminar tareas.
+- Un operador:
+    - No puede editar ni eliminar usuarios, solo verlos.
+    - No puede ver tableros que no le hayan sido asignados.
+    - Puede crear un tablero.
+    - No puede ver tareas que no le hayan sido asignadas.
+    - Puede crear tareas.
+    - Puede editar y eliminar tareas únicamente si le fueron asignadas.
+
+---
+
+
+## Corregir:
+- En vez de escribir las funciones isLogger e isAdmin en cada controller usar un helper, en una clase estática.
+- Que pide required en formularios cuando no esta solicitado en el viewmodel
+- Hacer funcion Login en Usuario Repository
+- Cualquier usuario puede crear tablero
+    - (Un usuario operario puede crear tableros y tareas propias, y puede asignar tareas propias a ajenas
+    tableros pueden hacer todos
+    y solamente pueden crearlos para uno, es decir, no se puede crear un tablero y asignarle el tablero a otro usuario
+    no importa si se es operario o admin)
+- Advertir al usuario de los errores del try catch
+
+---
+
+## Se puede agregar:
+Ofrecer colores y poder agregar colores en tareas
+
+---
+
+## Consultar:
+- Problema del selected en los formularios de editar tarea y usuario (estado y rol) (basically cuando toco editar, no se carga el dato, se pone el marcado en el selected)
+    - (solucion momentanea, quitar el selected del editar usuario)
+    - (segunda solucion momentanea, poner el selected en el primer valor)
+
+
+
+### Para Proyecto Final únicamente.
+Implemente la funcionalidad de asignar usuarios a tareas. El funcionamiento debería ser el siguiente:
+- c. El usuario logueado debe poder asignar un usuario a las tareas de las que es propietario.
+
+- d. El usuario logueado debería poder ver en la lista de tableros, además de los tableros que le pertenecen, todos los tableros donde tenga tareas asignadas.
+
+Los permisos del usuario logueado para tableros que no le pertenecen son:
+- Tableros: Solo lectura
+- Tareas no asignadas: Solo lectura.
+- Tareas asignadas: Lectura y modificación, pero únicamente para modificar el estado de la tarea.
