7231e35db6ce6ce86f6590391af06287e77451a8
Juani Gramajo <juanigramajo@hotmail.com>
2024-02-18 22:05:59 -0500

    opciones de tableros en tareas

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace tl2_tp10_2023_juanigramajo.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
